b463fa39f0c7b068c93dcf7ea9fae60dee08a37c
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 17:26:59 -0300

    table

--- index.sql
+++ index.sql
@@ -2,3 +2,18 @@
 
 --Criar Tabela
 
+CREATE TABLE Produtos(
+
+        ID INT PRIMARY KEY,
+        Nome VARCHAR(100),
+        Preço DECIMAL(10,2),
+        Quantidade INT
+);
+
+--Inserir dados da tabela
+
+INSERT INTO Produtos (ID, Nome, Preço, Quantidade) VALUES
+
+(1, 'camiseta', 19.99, 100 ),
+(2, 'calça Jeans', 29.99, 50 ),
+(3, 'Sapato', 49.99, 30 );
